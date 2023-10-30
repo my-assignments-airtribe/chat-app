50166d9eee9343d0e1ae6890712d902206129547
Ashrith Prakash <ashrith@lostinkaos.com>
2023-10-30 22:16:36 +0530

    server setup

---
+++ src/server/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('join', (room) => {
+    console.log('user joined room:', room);
+    socket.join(room);
+  });
+
+  socket.on('message', (message) => {
+    console.log('message:', message);
+    io.to(message.room).emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
