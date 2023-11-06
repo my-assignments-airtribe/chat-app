47781311756d35536105b8132aaabedfd396e9ec
Ashrith Prakash <ashrith@lostinkaos.com>
2023-11-06 17:52:16 +0530

    modal styles added to view files

--- src/client/components/file-display.tsx
+++ src/client/components/file-display.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import socket from './socket';
+import Modal from './modal';
 
 const FileDisplay:React.FC<{
   userId: string;
@@ -7,6 +8,7 @@ const FileDisplay:React.FC<{
   const [file, setFile] = useState<File | Blob>();
   const [fileType, setFileType] = useState('');
   const [fileUserId, setFileUserId] = useState<string>('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     socket.on("file", ({filename, data, username, timestamp, userId}) => {
@@ -33,20 +35,29 @@ const FileDisplay:React.FC<{
     };
   }, []);
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+
   const renderFile = () => {
     if (fileType === 'image') {
-      return <div className={`${userId === fileUserId ? "current-user" : "other-user"} image-upload`}>
+      return <div className={`${userId === fileUserId ? "current-user" : "other-user"} image-upload`} onClick={openModal}>
         <img src={file && URL.createObjectURL(file)} alt="Received Image" />
       </div>;
     } else if (fileType === 'pdf') {
       return (
-        <div className='pdf-upload'>
+        <div className='pdf-upload' onClick={openModal}>
           <embed src={file && URL.createObjectURL(file)} type="application/pdf" width="100%" height="500px" />
         </div>
       );
     } else if (fileType === 'document') {
       return (
-        <div className='document-upload'>
+        <div className='document-upload' onClick={openModal}>
           <iframe src={file && URL.createObjectURL(file)} width="100%" height="500px"></iframe>
         </div>
       );
@@ -58,6 +69,18 @@ const FileDisplay:React.FC<{
   return (
     <>
       {file && renderFile()}
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        {/* Render the file content inside the modal */}
+        {fileType === 'image' ? (
+          <img src={file && URL.createObjectURL(file)} alt="Received Image" />
+        ) : fileType === 'pdf' ? (
+          <embed src={file && URL.createObjectURL(file)} type="application/pdf" width="100%" height="500px" />
+        ) : fileType === 'document' ? (
+          <iframe src={file && URL.createObjectURL(file)} width="100%" height="500px"></iframe>
+        ) : (
+          <p>Unsupported file type</p>
+        )}
+      </Modal>
     </>
   );
 };
